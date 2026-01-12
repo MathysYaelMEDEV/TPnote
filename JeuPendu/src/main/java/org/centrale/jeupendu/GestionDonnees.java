508e146468c33031314827d3ad3c3eca2ff24fcf
Mathys INFOSI <56838862+Wygoo@users.noreply.github.com>
2026-01-12 15:50:34 +0100

    Gestion des données
    fonction motAlea() pour mode 1 joueur

--- JeuPendu/src/main/java/org/centrale/jeupendu/GestionDonnees.java
+++ JeuPendu/src/main/java/org/centrale/jeupendu/GestionDonnees.java
@@ -4,10 +4,37 @@
  */
 package org.centrale.jeupendu;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Random;
+
+
 /**
  *
  * @author dytri
  */
 public class GestionDonnees {
+    
+     /**
+     * Lit un fichier texte et retourne la liste de mots (1 mot par ligne)
+     *
+     * @return StringList contenant tous les mots
+     * @throws IOException si le fichier n'existe pas ou ne peut pas être lu
+     */
+    public static String motAlea() throws IOException {
+        String cheminFichier = "src/main/data/dico.txt";
+        Path path = Path.of(cheminFichier);
+        
+        Random random = new Random();
+
+        List<String> mots = Files.readAllLines(path);
+        int index = random.nextInt(mots.size());
+        
+        String mot = mots.get(index);
+        
+        return mot;
+    }
     
 }
--- JeuPendu/src/test/java/org/centrale/jeupendu/JeuPenduTest.java
+++ JeuPendu/src/test/java/org/centrale/jeupendu/JeuPenduTest.java
@@ -5,12 +5,9 @@
 package org.centrale.jeupendu;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
